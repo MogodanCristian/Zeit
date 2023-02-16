737f29505f4178886c6e7274dfdb0b2d96b7f635
Cristian Mogodan <mogodancristian1@gmail.com>
2023-02-16 17:53:48 +0200

    Created a basic header and removed the whitespace above it

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
 import { useState } from 'react'
+import Sidebar from './components/Sidebar'
+import { 
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import { createGlobalStyle }from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+  }
+`;
 
 function App() {
 
   return (
-    <div>MESAJ</div>
+    <Router>
+      <GlobalStyle/>
+        <Sidebar/>
+        <Routes>
+            <Route path='/' element = {<Dashboard/>}/>
+        </Routes>
+    </Router>
   )
 }
 
