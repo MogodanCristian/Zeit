d6509af817eda085759b45c439bae99892b8c801
Cristian Mogodan <mogodancristian1@gmail.com>
2023-02-16 20:19:48 +0200

    Sidebar done.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
+
 import { useState } from 'react'
-import Sidebar from './components/Sidebar'
+import Sidebar from './components/Sidebar/Sidebar'
 import { 
   BrowserRouter as Router,
   Navigate,
@@ -10,8 +11,12 @@ import Dashboard from './pages/Dashboard'
 import { createGlobalStyle }from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Lato&display=swap');
   body {
     margin: 0;
+    padding:0;
+    font-family: 'Lato', sans-serif;
+    box-sizing: border-box;
   }
 `;
 
---
+++ src/components/Sidebar/SidebarData.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import * as IoIcons from 'react-icons/io';
+
+export const SidebarData = [
+  {
+    title: 'Home',
+    path: '/',
+    icon: <AiIcons.AiFillHome />,
+  },
+  {
+    title: 'Projects',
+    path: '/reports',
+    icon: <IoIcons.IoIosPaper />,
+  },
+  {
+    title: 'Team',
+    path: '/team',
+    icon: <IoIcons.IoMdPeople />,
+  },
+  {
+    title: 'Support',
+    path: '/support',
+    icon: <IoIcons.IoMdHelpCircle />,
+    cName: 'nav-text'
+  }
+];
