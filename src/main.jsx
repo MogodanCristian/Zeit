b9a308933e9e6c172b3eb520c396d8b911fc3ae4
Cristian Mogodan <mogodancristian1@gmail.com>
2023-03-01 00:57:38 +0200

    Deleted the router from main

--- src/main.jsx
+++ src/main.jsx
@@ -8,8 +8,6 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <BrowserRouter>
       <App/>
-    </BrowserRouter>
   </Provider>
 )
