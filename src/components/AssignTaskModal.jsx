1d8ef5233327d16a98d5cd38608363eb9e25ea7a
Cristian Mogodan <mogodancristian1@gmail.com>
2023-04-25 00:46:40 +0300

    The employee that has it's task already assigned is shown with a specific span.Also alternates while selecting employees

--- src/components/AssignTaskModal.jsx
+++ src/components/AssignTaskModal.jsx
@@ -12,32 +12,67 @@ const EmployeeBox = styled.div`
   border-radius: 5px;
   padding: 5px 10px;
   margin-bottom: 10px;
+  height: 50px;
 `
 
-const AssignTaskModal = ({ show, onHide , _id}) => {
+const AssignTaskModal = ({ show, onHide, _id }) => {
   const env = JSON.parse(JSON.stringify(import.meta.env));
   const apiUrl = env.VITE_ZEIT_API_URL;
-  
-  const token = useSelector((state) => state.user.jwt)
 
-  const [employees, setEmployees] = useState([])
+  const token = useSelector((state) => state.user.jwt);
+
+  const [employees, setEmployees] = useState([]);
+  const [assignedEmployeeId, setAssignedEmployeeId] = useState(null);
+  const [selectedEmployee, setSelectedEmployee] = useState(null);
+  const [task, setTask] = useState(null);
+
   useEffect(() => {
     const config = {
-      headers: { 'auth-token': token }
+      headers: { 'auth-token': token },
     };
-    const path = 'http://localhost:3000/api/tasks/getProject/'+ _id
-    axios.get(path, config).then(response => {
-      const project = response.data
-      axios.get('http://localhost:3000/api/projects/getAvailableEmployees/' + project._id).then(availableRes =>{
-        setEmployees(availableRes.data)
-      }).catch(error => {
-      console.log(error);
-    });
-    }).catch(error => {
-      console.log(error);
-    });
-  }, [])
-  
+    const path = 'http://localhost:3000/api/tasks/getProject/' + _id;
+    axios
+      .get(path, config)
+      .then((response) => {
+        const project = response.data;
+        axios
+          .get(apiUrl + '/tasks/' + _id, config)
+          .then((detailsRes) => {
+            setTask(detailsRes.data);
+            setAssignedEmployeeId(detailsRes.data[0]?.assigned_to);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+
+        axios
+          .get(
+            'http://localhost:3000/api/projects/getAvailableEmployees/' +
+              project._id
+          )
+          .then((availableRes) => {
+            setEmployees(availableRes.data);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const handleAssignClick = (employee) => {
+    setSelectedEmployee(employee);
+    setAssignedEmployeeId(null);
+  };
+
+  const isEmployeeSelected = (employee) => {
+    if (employee._id === assignedEmployeeId) {
+      return true;
+    }
+    return employee === selectedEmployee;
+  };
 
   return (
     <Modal show={show} onHide={onHide}>
@@ -45,11 +80,19 @@ const AssignTaskModal = ({ show, onHide , _id}) => {
         <Modal.Title>Assign Task</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        {
-          employees.map((item) => (
-            <EmployeeBox>
-              <div>{item.first_name} {item.last_name}</div>
-            </EmployeeBox>
+        {employees.map((employee) => (
+          <EmployeeBox key={employee.id}>
+            <div>
+              {employee.first_name} {employee.last_name}
+            </div>
+            {isEmployeeSelected(employee) ? (
+              <span style={{ color: 'green'}}>✓ Assigned!</span>
+            ) : (
+              <Button variant="primary" onClick={() => handleAssignClick(employee)}>
+                Assign
+              </Button>
+            )}
+          </EmployeeBox>
         ))}
       </Modal.Body>
       <Modal.Footer>
@@ -61,7 +104,8 @@ const AssignTaskModal = ({ show, onHide , _id}) => {
         </Button>
       </Modal.Footer>
     </Modal>
-  )
-}
+  );
+};
 
-export default AssignTaskModal
+export default AssignTaskModal;
+
